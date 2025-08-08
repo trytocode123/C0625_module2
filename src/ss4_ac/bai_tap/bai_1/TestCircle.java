3766ccc6631515d3f1523dd9545557a9c3db818b
trytocode <annguyendang.17.07.2002@gmail.com>
2025-08-08 18:29:24 +0700

    update

--- src/ss4_ac/bai_tap/bai_1/TestCircle.java
+++ src/ss4_ac/bai_tap/bai_1/TestCircle.java
@@ -4,6 +4,7 @@ public class TestCircle {
     public static void main(String[] args) {
         Cirle cirle = new Cirle(10.0);
         cirle.getArea();
+        cirle.setColor("blue");
         System.out.println(cirle);
     }
 }
