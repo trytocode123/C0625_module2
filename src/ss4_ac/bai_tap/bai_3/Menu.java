d982b71c73132ee5f522ef90ee10ce47194a6f54
trytocode <annguyendang.17.07.2002@gmail.com>
2025-08-10 22:56:16 +0700

    update

--- src/ss4_ac/bai_tap/bai_3/Menu.java
+++ src/ss4_ac/bai_tap/bai_3/Menu.java
@@ -1,6 +1,5 @@
 package ss4_ac.bai_tap.bai_3;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Menu {
@@ -9,89 +8,48 @@ public class Menu {
     final static int UPDATE = 3;
     final static int DELETE = 4;
     final static int SEARCH = 5;
+    final static int EXIT = 6;
 
     public void display() {
         boolean flag = true;
         while (flag) {
-            System.out.println("-----Quản lý sản phẩm-----\n 1. Hiển thị danh sách\n 2. Thêm sản phầm\n 3. Cập nhật sản phẩm\n 4. Xóa sản phẩm\n 5. Tìm kiếm sản phẩm");
+            System.out.println("-----Quản lý sản phẩm-----\n 1. Hiển thị danh sách\n 2. Thêm sản phầm\n 3. Cập nhật sản phẩm\n 4. Xóa sản phẩm\n 5. Tìm kiếm sản phẩm\n 6. Thoát");
             Scanner sc = new Scanner(System.in);
             int option = Integer.parseInt(sc.nextLine());
             ProductManager pm = new ProductManager();
-            Product[] list = pm.getAll();
+
             switch (option) {
                 case DISPLAY:
+                    Product[] list = pm.getAll();
                     System.out.println("Đây là chức năng hiển thị danh sách sản phẩm");
-
                     for (Product product : list) {
                         System.out.println(product);
                     }
                     break;
                 case ADD:
                     System.out.println("Đây là chức năng thêm sản phẩṃ");
-                    System.out.println("Nhập id sản phẩm");
-                    int iD = Integer.parseInt(sc.nextLine());
-                    boolean flagAdd = false;
-                    for (Product item : list) {
-                        if (item.getID() == iD) {
-                            System.out.println("Sản phẩm đã tồn tại");
-                            flagAdd = true;
-                            break;
-                        }
-                    }
-                    if (!flagAdd) {
-                        System.out.println("Nhập id sản phẩm");
-                        iD = Integer.parseInt(sc.nextLine());
-                        System.out.println("Nhập tên sản phẩm");
-                        String name = sc.nextLine();
-                        System.out.println("Nhập giá sản phẩm");
-                        int price = Integer.parseInt(sc.nextLine());
-                        pm.add(new Product(iD, price, name));
-                        for (int i = 0; i < pm.getAll().length; i++) {
-                            System.out.println(pm.getAll()[i]);
-                        }
-                    }
-
+                    pm.add();
                     break;
 
                 case UPDATE:
                     System.out.println("Đây là chức năng sửa sản phẩṃ");
                     pm.update();
-                    for (int i = 0; i < pm.getAll().length; i++) {
-                        System.out.println(pm.getAll()[i]);
-                    }
                     break;
 
                 case SEARCH:
                     System.out.println("Đây là chức năng tìm kiếm sản phẩṃ");
-                    Product productSearch = pm.search();
-                    if (productSearch == null) {
-                        System.out.println("Sản phầm không tồn tại");
-                    } else {
-                        System.out.println(productSearch);
-                    }
+                    pm.search();
                     break;
 
                 case DELETE:
                     System.out.println("Đây là chức năng xóa sản phẩm");
-                    Product[] deleteList = pm.delete();
-                    if (deleteList == null) {
-                        System.out.println("Không tồn tại sản phẩm");
-                    } else {
-                        System.out.println("Xóa sản phẩm thành công");
-                        for (Product product : deleteList) {
-                            if (product != null) {
-                                System.out.println(product);
-                            }
-
-                        }
-                    }
+                    pm.delete();
                     break;
 
-                default:
+                case EXIT:
                     flag = false;
             }
         }
 
     }
-
 }
--- src/ss4_ac/bai_tap/bai_3/ProductManager.java
+++ src/ss4_ac/bai_tap/bai_3/ProductManager.java
@@ -35,74 +35,108 @@ public class ProductManager {
         return products;
     }
 
-    public void add(Product product) {
-        for (int i = 0; i < productList.length; i++) {
-            if (productList[i] == null) {
-                productList[i] = product;
-                System.out.println("Đã thêm thành công");
+    public void add() {
+        System.out.println("Nhập id sản phẩm");
+        Scanner sc = new Scanner(System.in);
+        int iD = Integer.parseInt(sc.nextLine());
+        boolean flagAdd = false;
+        for (Product item : productList) {
+            if (item != null && item.getID() == iD) {
+                System.out.println("Sản phẩm đã tồn tại");
+                flagAdd = true;
                 break;
-            } else {
+            }
+        }
+        boolean flagIsFull = false;
+        if (!flagAdd) {
+            System.out.println("Nhập tên sản phẩm");
+            String name = sc.nextLine();
+            System.out.println("Nhập giá sản phẩm");
+            int price = Integer.parseInt(sc.nextLine());
+            for (int i = 0; i < productList.length; i++) {
+                if (productList[i] == null) {
+                    productList[i] = new Product(iD, price, name);
+                    System.out.println("Đã thêm thành công");
+                    flagIsFull = true;
+                    break;
+                }
+            }
+            if (!flagIsFull) {
                 System.out.println("Danh sách đã đầy, không thể thêm");
             }
         }
+        Product[] list = this.getAll();
+
+        for (Product item : list) {
+            System.out.println(item);
+        }
+
+    }
+
+    private int positionItemAvailable() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập id sản phẩm xem sản phẩm có tồn tại hay không");
+        int iD = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < productList.length; i++) {
+            if (productList[i] != null && productList[i].getID() == iD) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public void update() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập id sản phẩm");
-        int iDToFind = Integer.parseInt(sc.nextLine());
-        boolean flag = false;
-        for (int i = 0; i < productList.length; i++) {
-            if (productList[i] != null && productList[i].getID() == iDToFind) {
-                System.out.println("Nhập id sản phẩm");
-                int iD = Integer.parseInt(sc.nextLine());
-                System.out.println("Nhập tên sản phẩm");
-                String name = sc.nextLine();
-                System.out.println("Nhập giá sản phẩm");
-                int price = Integer.parseInt(sc.nextLine());
-                productList[i] = new Product(iD, price, name);
-                System.out.println("Cập nhật thành công");
-                flag = true;
-                break;
-            }
-        }
-
-        if (!flag) {
+        int index = this.positionItemAvailable();
+        if (index != -1) {
+            System.out.println("Nhập id sản phẩm để câp nhật");
+            int iD = Integer.parseInt(sc.nextLine());
+            System.out.println("Nhập tên sản phẩm");
+            String name = sc.nextLine();
+            System.out.println("Nhập giá sản phẩm");
+            int price = Integer.parseInt(sc.nextLine());
+            productList[index] = new Product(iD, price, name);
+            System.out.println("Cập nhật thành công");
+        } else {
             System.out.println("Không tồn tại sản phẩm");
         }
+
+        Product[] list = this.getAll();
+        for (Product i : list) {
+            System.out.println(i);
+        }
+
     }
 
-    public Product search() {
-        System.out.println("Nhập id để tìm kiếm sản phẩm");
-        Scanner sc = new Scanner(System.in);
-        int iD = Integer.parseInt(sc.nextLine());
-        for (Product product : productList) {
-            if (product != null && product.getID() == iD) {
-                return product;
-
-            }
+    public void search() {
+        int positionItem = positionItemAvailable();
+        if (positionItem != -1) {
+            System.out.println(productList[positionItem]);
+        } else {
+            System.out.println("Sản phẩm không được tìm thấy");
         }
-        return null;
     }
 
-    public Product[] delete() {
-        Product[] newList;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập id cần xóa");
-        int iD = Integer.parseInt(sc.nextLine());
+    public void delete() {
+        int position = positionItemAvailable();
+        if (position != -1) {
+            Product[] newList;
 
-        for (int i = 0; i < productList.length; i++) {
-            if (productList[i] != null && productList[i].getID() == iD) {
-                for (int j = i; j < productList.length - 1; j++) {
-                    productList[j] = productList[j + 1];
-                }
-                newList = this.getAll();
-                if (newList.length > 1 && newList[newList.length - 1] == newList[newList.length - 2]) {
-                    newList[newList.length - 1] = null;
-                }
-                return newList;
+            for (int j = position; j < productList.length - 1; j++) {
+                productList[j] = productList[j + 1];
             }
+            newList = this.getAll();
+            if (newList.length > 1 && newList[newList.length - 1] == newList[newList.length - 2]) {
+                newList[newList.length - 1] = null;
+            }
+
+            System.out.println("Xóa sản phẩm thành công");
+            for (Product product : newList) {
+                System.out.println(product);
+            }
+
+        } else {
+            System.out.println("Không tồn tại sản phẩm");
         }
-        return null;
     }
 }
