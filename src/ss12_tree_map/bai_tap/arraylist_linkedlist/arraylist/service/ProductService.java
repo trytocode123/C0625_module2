215d7f86edc459402f3eb22d31cde9aa0a6f0c67
trytocode <annguyendang.17.07.2002@gmail.com>
2025-08-22 20:48:15 +0700

    update

--- src/ss12_tree_map/bai_tap/arraylist_linkedlist/arraylist/service/ProductService.java
+++ src/ss12_tree_map/bai_tap/arraylist_linkedlist/arraylist/service/ProductService.java
@@ -34,10 +34,7 @@ public class ProductService implements IProductService {
         return productRepository.isValid(iD);
     }
 
-    public void sortIncrease(List<Product> productList) {productRepository.sortIncrease(productList);
-    }
+    public void sortIncrease(List<Product> productList) {productRepository.sortIncrease(productList);}
 
-    public void sortDecrease(List<Product> productList) {productRepository.sortDecrease(productList);
-    }
-
+    public void sortDecrease(List<Product> productList) {productRepository.sortDecrease(productList);}
 }
