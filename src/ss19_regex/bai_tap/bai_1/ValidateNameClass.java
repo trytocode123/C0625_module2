3a818dcd7ca332d4fc77dcc64992520f6b8832d2
trytocode <annguyendang.17.07.2002@gmail.com>
2025-08-25 16:39:41 +0700

    update

---
+++ src/ss19_regex/bai_tap/bai_1/ValidateNameClass.java
@@ -0,0 +1,22 @@
+package ss19_regex.bai_tap.bai_1;
+
+import java.util.Scanner;
+
+public class ValidateName {
+    public static void main(String[] args) {
+        System.out.println("Enter class name to check: ");
+        Scanner sc = new Scanner(System.in);
+        String className = sc.nextLine();
+        if (checkClassName(className)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not valid");
+        }
+    }
+
+    public static boolean checkClassName(String className) {
+        String regex = "^[CAP]\\d{4}[GHIK]$";
+        return className.matches(regex);
+    }
+
+}
